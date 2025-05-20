680319e3f0db599babaa9935cff090e4f74e0b1b
ffrndzTech <199731008+ffernandezTech@users.noreply.github.com>
2025-05-19 23:43:47 -0700

    feat: basic Arithmetic funcitons

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function getAddition(arr1, arr2)
+{
+    return arr1+arr2;
+}
+function getSubtraction(arr1, arr2)
+{
+    return arr1-arr2;
+}
+function getMultiplication{
+    return arr1*arr2;
+}
+function getDivision{
+    return arr1/arr2;
+}
+
