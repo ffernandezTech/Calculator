fde1dbdcaa109478054494a098eef9ec10181d4c
ffrndzTech <199731008+ffernandezTech@users.noreply.github.com>
2025-05-24 00:28:46 -0700

    feat: added code for if user wants to continue using the result and if not

--- javascript.js
+++ javascript.js
@@ -21,14 +21,14 @@ function getOperate(operation, number1, number2){
 
     //using more standard symbols for each operate.
     switch(operation){
-        case '+': displayLine.textContent = getAddition(number1, number2);
+        case '+': return displayLine.textContent = getAddition(number1, number2);
         break;
-        case '-': displayLine.textContent =getSubtraction(number1,number2);
+        case '-': return displayLine.textContent =getSubtraction(number1,number2);
         break;
-        case 'x':displayLine.textContent =getMultiplication(number1, number2);
+        case 'x': return displayLine.textContent =getMultiplication(number1, number2);
         break;
         //need to copy paste this divison symbol.
-        case '÷':displayLine.textContent= getDivision(number1,number2);
+        case '÷': return displayLine.textContent= getDivision(number1,number2);
         break;
 
     }
@@ -89,6 +89,9 @@ btnOps.forEach(btnOperation=>{
 const btnNumPad = document.querySelectorAll('.btn');
 let num1 = '';
 let num2 = '';
+
+//Using to keep track of the number after pressing equal
+let count = 0;
 // console.log(btnCall);
 
 //For each btn in our numpad we add the text content into a string
@@ -96,18 +99,23 @@ btnNumPad.forEach(btnNum=> {
     
     btnNum.addEventListener('click',(e)=>{
 
+        if(operations==='' && count==1)
+        {
+            count=0;
+            num1='';
+        }
 
         if(displayLine.textContent.length > 20)
         {
             displayLine.style.fontSize = '25px';
         }
     
-        if(operations === '')
+        if(operations === '' && count ===0 )
         {
             num1+= btnNum.textContent;
             setValues(num1);
         }
-        else 
+        else if(operations != '')
         {
             num2 += btnNum.textContent;
             setValues(btnNum.textContent);
@@ -143,6 +151,7 @@ const btnEqual = document.querySelector('.btnEqual')
 
 btnEqual.addEventListener('click',()=>{
 
+   
     if(operations === '÷' && parseInt(num2) === 0 )
     {
         alert('CANT DIVIDE BY ZERO');
@@ -150,8 +159,16 @@ btnEqual.addEventListener('click',()=>{
 
     }
     else{
-        getOperate(operations, parseInt(num1), parseInt(num2));
+        console.log(`first number ${num1} with this operation ${operations} and finally this final number ${num2}`)
+        num1 = getOperate(operations, parseInt(num1), parseInt(num2));
+        console.log(`THIS IS THE NUMBER 1 after operation ${num1}`);
+        count = 1;
+        operations = '';
+        num2 = '';
+       
     }
+
+    
 
 
 
