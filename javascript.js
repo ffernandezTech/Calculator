795be0efbc3a9a69720d595410a33f6cb96d5264
ffrndzTech <199731008+ffernandezTech@users.noreply.github.com>
2025-06-10 01:50:16 -0700

    fixed: decimal saves to leading second number is required

--- javascript.js
+++ javascript.js
@@ -74,8 +74,9 @@ function dblCheck()
             // console.log(`THIS IS THE NUMBER 1 after operation ${num1}`);
 
 
-
-            count = 1;
+            //newNumber is used to check that the user is entering a different number or
+            //still using the same number after solution.
+            newNumber = true;
             num2 = '';
         }
     else{
@@ -87,7 +88,9 @@ function dblCheck()
             // console.log(`THIS IS NUMBER 1 after operation ${num1}`);
 
 
-            count = 1;
+          //newNumber is used to check that the user is entering a different number or
+            //still using the same number after solution.
+            newNumber = true;
             num2 = '';
         }
         
@@ -137,10 +140,17 @@ btnOps.forEach(btnOperation=>{
         {
             if(num2==='')
             {
+
+                //if no number is entered in Num2
+                //replace operation with the new entered valued.
                 operations=btnOperation.textContent;
+                decimalOPsTracker++;
             }
             else{
+
+                //if number 2 is not empty solve
                 dblCheck();
+                decimalOPsTracker++;
                 operations= btnOperation.textContent;
                 // operations= btnOperation.textContent;
             }
@@ -169,19 +179,23 @@ const btnNumPad = document.querySelectorAll('.btn');
 let num1 = '';
 let num2 = '';
 
-let numArray = [];
 //Using to keep track of the number after pressing equal
-let count = 0;
-// console.log(btnCall);
+
+//newNumber is used to check that the user is entering a different number or
+//still using the same number after solution.
+let newNumber = false;
 
 //For each btn in our numpad we add the text content into a string
 btnNumPad.forEach(btnNum=> {
     
     btnNum.addEventListener('click',(e)=>{
 
-        if(operations==='' && count==1)
+        if(operations==='' && newNumber === true)
         {
-            count=0;
+            newNumber=false;
+            //If the user hasnt entered a new operation after solving
+            //and instead entered a number that number is now suppose to be
+            //the new number 1.
             num1='';
             
         }
@@ -191,17 +205,25 @@ btnNumPad.forEach(btnNum=> {
             displayLine.style.fontSize = '36px';
         }
     
-        if(operations === '' && count ===0 )
+        if(operations === '' && newNumber ===false )
         {
             num1+= btnNum.textContent;
+
             setDisplay(num1);
-            console.log(`This is the first number ${num1}`)
+            
+            
+            // console.log(`This is the first number ${num1}`)
         }
         else if(operations != '')
         {
             num2 += btnNum.textContent;
+
+
+            
             setDisplay(num2);
-             console.log(`This is the second number ${num2}`)
+            
+            
+            // console.log(`This is the second number ${num2}`)
         }
 
     
@@ -234,8 +256,10 @@ const btnDecimal = document.querySelector('.btnDecimal')
 btnDecimal.addEventListener('click',()=>{
 
    
-    //NEEDS TO WORK IF USER ENTERS .02.
-    if(num1!=''&& num2==='')
+    //decimalOPsTracker is to make sure that the user can enter a starting
+    //decimal for the second number and it wont be placed at the end of the first number
+    
+    if(decimalOPsTracker===0)
     {
        
 
@@ -252,6 +276,7 @@ btnDecimal.addEventListener('click',()=>{
             num2 = num2+'.';
          setDisplay(num2);
         }
+        decimalOPsTracker=0;
     }
 
 
